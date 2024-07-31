713e500cfe15834d4478b1f61373cd6de90dd178
magistraapta <magsitraagis@gmail.com>
2024-07-31 11:27:02 +0800

    Initial Commit

---
+++ pomo-timer/pomo_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomo_timerApp.swift
+//  pomo-timer
+//
+//  Created by Magistra Apta on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct pomo_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
